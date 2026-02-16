bed23251a20ac9902ee2002030d2bcb10c186fab
walkwelrajan238 <walkwelrajan@gmail.com>
2026-02-16 16:27:58 +0530

    Auth  module setup in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AppConfigModule } from "src/services/env/env.module";
+import { AuthModule } from "src/api/auth/auth.module";
 
 @Module({
-    imports: [AppConfigModule],
+    imports: [AppConfigModule, AuthModule],
     controllers: [AppController],
     providers: [AppService]
 })
