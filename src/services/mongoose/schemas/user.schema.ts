31931599a9e294af8bdc3322cb4b25754236cec4
walkwelrajan238 <walkwelrajan@gmail.com>
2026-02-17 17:17:25 +0530

    user schema created

--- src/services/mongoose/schemas/user.schema.ts
+++ src/services/mongoose/schemas/user.schema.ts
@@ -1,10 +1,55 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import commonFieldsPlugin from "../plugins/common-fields";
+import { ApiProperty } from "@nestjs/swagger";
+import { Types } from "mongoose";
+import { Role } from "./role.schema";
 
-@Schema({ collection: 'users' })
-export class User {}
+@Schema({ collection: "users" })
+export class User {
+  @ApiProperty()
+  @Prop({
+    required: true,
+    type: String,
+    unique: true,
+  })
+  _id: string;
+
+  @ApiProperty()
+  @Prop()
+  firstName: string;
+
+  @ApiProperty()
+  @Prop()
+  lastName: string;
+
+  @ApiProperty()
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @ApiProperty()
+  @Prop({
+    unique: true,
+    sparse: true,
+  })
+  phoneNumber: string;
+
+  @ApiProperty()
+  @Prop({
+    required: false,
+  })
+  password: string;
+
+  @ApiProperty({ type: [String] })
+  @Prop({
+    type: [Types.ObjectId],
+    ref: "Role",
+  })
+  role: Role[];
+}
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.plugin(commonFieldsPlugin, { name: User.name });
-    
